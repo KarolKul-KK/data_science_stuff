6d851335c3d160abc958c1d0d36de8e54f9c4c07
KarolKul-KK <>
2021-05-20 20:52:31 +0200

    master

---
+++ way_to_load_data/load_from_csv.py
@@ -0,0 +1,12 @@
+import csv
+
+
+with open('market_data.txt') as f:
+    tab_reader = csv.reader(f, delimiter='\t')
+    for row in tab_reader:
+        date = row[0]
+        symbol = row[1]
+        closing_price = float(row[2])
+        process(date, symbol, closing_price)
+
+print(f)
